e740b9aa9e63f863f55c9283167146662a942fb8
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-12-06 13:18:08 -0500

    Displays a random question on index page

--- server.rb
+++ server.rb
@@ -2,11 +2,26 @@ require 'sinatra'
 require "sinatra/activerecord"
 require 'sinatra/reloader'
 
+class String
+  def is_i?
+    /\A[-+]?\d+\z/ === self
+  end
+end
+
 require_relative 'models/question'
 also_reload 'models/question'
 
-
 get '/' do
-  @questions = Question.all
-  erb :index
+  redirect "/questions/#{rand(Question.count+1)}"
+end
+
+get '/questions/:id' do
+  if params[:id].is_i? && params[:id].to_i <= Question.count
+    id = params[:id]
+  else
+    redirect "/questions/#{rand(Question.count+1)}"
+  end
+
+  @question = Question.find(id)
+  erb :show
 end
