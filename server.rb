af6e92424971fc6e86ece357b58b753f3d5b21c7
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-12-06 15:44:48 -0500

    Questions display over crazy-hair-guy image.

--- server.rb
+++ server.rb
@@ -12,14 +12,14 @@ require_relative 'models/question'
 also_reload 'models/question'
 
 get '/' do
-  redirect "/questions/#{rand(Question.count+1)}"
+  redirect "/questions/#{rand(1..Question.count)}"
 end
 
 get '/questions/:id' do
   if params[:id].is_i? && params[:id].to_i <= Question.count
     id = params[:id]
   else
-    redirect "/questions/#{rand(Question.count+1)}"
+    redirect "/questions/#{rand(1..Question.count)}"
   end
 
   @question = Question.find(id)
